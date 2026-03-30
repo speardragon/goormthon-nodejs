b0a5ad8aafbf66578ea814e21d53d975deee0fe8
ray-goorm <ray.kang@goorm.io>
2026-03-30 15:56:09 +0900

    fix: goormthon-2 test

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello from NodeJS!' });
+app.get("/", (req, res) => {
+  res.json({ message: "Hello from NodeJS!" });
 });
 
 app.listen(PORT, () => {
